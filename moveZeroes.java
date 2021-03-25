f4d8f4ca3c7f4d7d2ce8c91e0fc992c623f8f8a0
Ryan Hoang <ryanhoang15@gmail.com>
2021-03-25 03:29:09 -0700

    Uploading solved leetcode problem using git command on terminal

---
+++ moveZeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                continue;
+            }
+            else{
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        for(int j = count; j < nums.length; j++){
+            nums[j] = 0;
+        }
+        
+    }
+}
